712798650ecb3a6b80099740e63f331b13e019b5
dimi9595 <dmitry.mindlin@gmail.com>
2021-12-03 09:48:35 +0100

    Adapted old code. And loaded Data

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,10 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='./datasets/flickr8k/dataset_flickr8k.json',
+                       image_folder='./datasets/flickr8k/images',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder='./datasets/flickr8k/output',
                        max_len=50)
